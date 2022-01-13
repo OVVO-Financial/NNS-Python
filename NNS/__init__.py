9f5a7ad55494d991bc51c432673044564cc65a69
Roberto Spadim <roberto@spadim.com.br>
2022-01-13 15:59:03 -0300

    isort

--- NNS/__init__.py
+++ NNS/__init__.py
@@ -3,24 +3,24 @@ from . import Internal_Functions
 from .Binary_ANOVA import NNS_ANOVA_bin
 from .Copula import NNS_copula
 from .FSD import NNS_FSD
-from .SSD import NNS_SSD
-from .TSD import NNS_TSD
-from .LPM_UPM_VaR import UPM_VaR, LPM_VaR
+from .LPM_UPM_VaR import LPM_VaR, UPM_VaR
 from .NNS_term_matrix import NNS_term_matrix
 from .Numerical_Differentiation import NNS_diff
-from .Partial_Moments import (
-    LPM,
-    UPM,
-    Co_UPM,
-    Co_LPM,
+from .Partial_Moments import (  # TODO: NNS_PDF
     D_LPM,
     D_UPM,
-    PM_matrix,
-    LPM_ratio,
-    UPM_ratio,
+    LPM,
     NNS_CDF,
-)  # TODO: NNS_PDF
+    UPM,
+    Co_LPM,
+    Co_UPM,
+    LPM_ratio,
+    PM_matrix,
+    UPM_ratio,
+)
 from .SD_Efficient_Set import NNS_SD_efficient_set
+from .SSD import NNS_SSD
+from .TSD import NNS_TSD
 from .Uni_SD_Routines import NNS_FSD_uni, NNS_SSD_uni, NNS_TSD_uni
 
 __version__ = "0.1.12"
